568359e61daea5277258738586d5257a9ce4393b
sergiovillagran <sergio.villagran@3dves.com>
2017-11-13 20:07:42 -0500

    lol

--- corte.py
+++ corte.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 datos = pd.read_csv('./Delito_Hurto_Celulares.csv')
 print(datos.head(50))
-print(datos.describre())
+#print(datos.describre())
 
-datosResumidos = datos[['Header1', 'Header2', 'Header3']]
+#datosResumidos = datos[['Header1', 'Header2', 'Header3']]
 print(datos.corr())
