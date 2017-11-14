a4232c050d9c14e66bce218ffe2f75b4c5b82f79
sergiovillagran <sergio.villagran@3dves.com>
2017-11-14 02:16:56 -0500

    different plot types

--- corte.py
+++ corte.py
@@ -1,15 +1,35 @@
-import pandas as pd 
 import matplotlib as mpl
+import pandas as pd 
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 
-datos = pd.read_csv('./Delito_Hurto_Celulares.csv')
-print(datos.head(50))
-#print(datos.describre())
+DATA    = pd.read_csv('Delito_Hurto_Celulares.csv', parse_dates= True, index_col= 'FECHA')
 
-#datosResumidos = datos[['Header1', 'Header2', 'Header3']]
-print(datos.corr())
+# scatter graph
+DATA['days'] = DATA.index.day
+DATA.plot.scatter('days', 'EDAD')
+FIG     = plt.gcf()
+FIG.savefig('scatter.png')
 
-fig = plt.gcf()
-datos.plot.scatter('DIA', 'HORA')
-fig.savefig('out.png')
+# histograma
+DATA['month'] = DATA.index.month
+DATA['month'].hist() 
+FIG     = plt.gcf()
+FIG.savefig('histograma.png')
+
+# box
+DATA.EDAD.plot.box()
+FIG     = plt.gcf()
+FIG.savefig('box.png')
+
+# otro 1 pie
+victimas_a_pie = DATA['MOVIL VICTIMA'] == "A PIE" 
+victimas_a_pie = DATA[victimas_a_pie]
+victimas_a_pie['MOVIL AGRESOR'].value_counts().plot.pie()
+FIG     = plt.gcf()
+FIG.savefig('pie.png')
+
+# otro 2 bar
+DATA['MARCA'].value_counts().plot.bar()
+FIG     = plt.gcf()
+FIG.savefig('bar.png')
