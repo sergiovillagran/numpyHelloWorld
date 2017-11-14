f0b61d7a3bf8a767311125122ffe6a21f65482d1
sergiovillagran <sergio.villagran@3dves.com>
2017-11-13 19:58:43 -0500

    test

--- GT_2.py
+++ GT_2.py
@@ -1,5 +1,5 @@
-import numpy as np
-import matplotlib as mpl
+import numpy             as np
+import matplotlib        as mpl
 mpl.use('Agg');
 import matplotlib.pyplot as plt
 anlos = np.loadtxt('anlos.txt', delimiter = ';')
---
+++ corte.py
@@ -0,0 +1,11 @@
+import pandas as pd 
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+
+datos = pd.read_csv("Delito_Hurto_Celulares.csv")
+print(datos.head(50))
+print(datos.describre())
+
+datosResumidos = datos[['Header1', 'Header2', 'Header3']]
+print(datos.corr())
